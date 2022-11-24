e26cd3615d2b47b955fa1f2d3bafb9d2646324e4
Marek Rożko <maknamar12@gmail.com>
2022-11-24 17:45:31 +0100

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
-console.log('Hello world');
+const startButton = document.querySelector('button[data-action = "start"]');
+const stopButton = document.querySelector('button[data-action = "stop"]');
+
+startButton.addEventListener('click', createColor);
+stopButton.addEventListener('click', stopChangeColor);
+stopButton.setAttribute('disabled', true);
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function activeButton() {
+  if (createColor) {
+    startButton.setAttribute('disabled', true);
+  }
+}
+
+function createColor() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  activeButton();
+  stopButton.removeAttribute('disabled', true);
+}
+
+function stopChangeColor() {
+  startButton.removeAttribute('disabled', true);
+  stopButton.setAttribute('disabled', true);
+  clearInterval(timerId);
+}
